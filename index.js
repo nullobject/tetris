d56587db1ca76517b3b9b134f94231152599f46d
Josh Bassett <josh.bassett@gmail.com>
2017-12-19 21:44:54 +1100

    Change to postcss

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-import './styles.scss'
 import 'tachyons'
-import {prepend} from 'fkit'
 import Game from './game'
-import {Signal, keyboard} from 'bulb'
 import choo from 'choo'
 import html from 'choo/html'
 import nanobus from 'nanobus'
 import nanologger from 'nanologger'
+import styles from './styles.css'
+import {Signal, keyboard} from 'bulb'
+import {prepend} from 'fkit'
 
 const CLOCK_PERIOD = 1000
 const UP = 38, DOWN = 40, LEFT = 37, RIGHT = 39
 
 const game = new Game()
 
-// window.initialState = {game}
+window.initialState = {game}
 
-const app = choo({initialState: {game}})
+const app = choo()
 
 if (process.env.NODE_ENV !== 'production') {
   app.use(require('choo-devtools')())
@@ -23,12 +23,32 @@ if (process.env.NODE_ENV !== 'production') {
 
 const log = nanologger('game')
 
-const view = (state, emit) => {
+function gameView (state, emit) {
   return html`
     <body class="sans-serif bg-hot-pink">
       <h1 class="f-headline pa3 pa4-ns">${state.game.toString()}</h1>
-      <button class="f5 dim br-pill ph3 pv2 mb2 dib black bg-white bn pointer" onclick=${() => emit('tick')}>hello</button>
+      <button class="f5 dim br-pill ph3 pv2 mb2 dib black bg-white bn pointer" onclick=${() => emit('tick')}>Pause</button>
+      ${tetrionView(state, emit)}
     </body>
+  `
+}
+
+function tetrionView (state, emit) {
+  return html`
+    <div class="${styles.tetrion}">
+      ${playfieldView(state, emit)}
+    </ul>
+  `
+}
+
+function playfieldView (state, emit) {
+  const blocks = [{x: 0, y: 0, c: "red"}, {x: 1, y: 0, c: "green"}]
+  return html`
+    <ul class="${styles.playfield}">
+      ${blocks.map(block =>
+        html`<li class="bg-${block.c}" style="bottom: 0px; left: ${block.x * 20}px;"></li>`
+      )}
+    </ul>
   `
 }
 
@@ -82,5 +102,5 @@ app.emitter.on('DOMContentLoaded', () => {
   }
 })
 
-app.route('/', view)
+app.route('/', gameView)
 app.mount(document.body)
