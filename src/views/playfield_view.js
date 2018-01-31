c25ba788e28270eca674df0586bd0f79e3730a12
Josh Bassett <josh.bassett@gmail.com>
2018-02-01 09:15:58 +1100

    Animate when clearing lines

--- src/views/playfield_view.js
+++ src/views/playfield_view.js
@@ -1,13 +1,21 @@
 import BlockView from './block_view'
 import React from 'react'
 import styles from '../../assets/stylesheets/styles.scss'
+import {CSSTransition, TransitionGroup} from 'react-transition-group'
 
 export default class PlayfieldView extends React.PureComponent {
   render () {
     const {blocks} = this.props.playfield
+
     return (
       <ul className={styles.playfield}>
-        {blocks.map(block => <BlockView key={block.id} block={block} />)}
+        <TransitionGroup>
+          {blocks.map(block => (
+            <CSSTransition key={block.id} timeout={1000} classNames={{exit: styles['fade-exit'], exitActive: styles['fade-exit-active']}}>
+              <BlockView block={block} />
+            </CSSTransition>
+          ))}
+        </TransitionGroup>
       </ul>
     )
   }
